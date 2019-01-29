16b0928281783113da39ddb6bbbf9be7a9159738
Sniper <black_strings0@hotmail.com>
2019-01-28 21:04:00 -0600

    add file

---
+++ Builder.cs
@@ -0,0 +1,26 @@
+class Builder {
+	
+	private int height;
+	public string name;
+
+	public Builder(int height){
+		this.height = height;
+		name = "sam";
+	}
+
+	public void repackage() {
+		if (this.height >= 10) {
+			finalize();
+		} else {
+			reverify();
+		}
+	}
+
+	public void finalize() {
+		console.log("finalizing...");
+	}
+
+	public void reverify() {
+		console.log("")
+	}
+}
